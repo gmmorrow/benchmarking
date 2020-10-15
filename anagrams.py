b88a714b66d5fe10d679310233a4ac7aee3096e3
gabrielle@Gabbys-MacPro.com <gab_morrow@yahoo.com>
2020-10-15 15:21:35 -0400

    final benchmark

--- anagrams.py
+++ anagrams.py
@@ -10,7 +10,7 @@ for an arbitrary list of strings.
 
 # Your name here, and any other people/sources who helped.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Gabrielle"
 
 import sys
 
@@ -27,12 +27,19 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    alphabet_words = {word: alphabetize(word) for word in words}
+    anagrams = {}
+    for key, value in alphabet_words.items():
+        if value in anagrams:
+            anagrams[value].append(key)
+        else:
+            anagrams[value] = [key]
     return anagrams
+
+    #     alphabetize(word): [
+    #         w for w in words
+    #         if alphabetize(w) == alphabetize(word)]
+    #     for word in words}
 
 
 def main(args):
